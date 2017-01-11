d69700e63cd802678e57a3f09a30896bf46b4aeb
Kevin <kevinmoore9@users.noreply.github.com>
2017-01-11 09:42:31 -0800

    heroku

--- frontend/instaclone.jsx
+++ frontend/instaclone.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root';
+
+import configureStore from './store/store';
+import { login } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>InstaClone</h1>, root);
+  const store = configureStore();
+  window.store = store;
+  window.login = login;
+  ReactDOM.render(<Root store={store} />, root);
+
 });

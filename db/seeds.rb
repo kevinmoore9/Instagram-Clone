969d322090bb61986a23c0efb7435bcf43212612
Kevin <kevinmoore9@users.noreply.github.com>
2017-01-13 11:00:08 -0800

    update seeds..again

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,18 +8,18 @@
 User.destroy_all
 Photo.destroy_all
 
-guest = User.create(username: "guest", password: "password",
+guest = User.create!(username: "guest", password: "password",
   profile_img_url: "https://images.pexels.com/photos/26504/pexels-photo.jpg?w=1260&h=750&auto=compress&cs=tinysrgb")
 
-sports_news = User.create(username: "sports_news", password: "espn",
+sports_news = User.create!(username: "sports_news", password: "espn123",
   profile_img_url: "https://images.pexels.com/photos/47730/the-ball-stadion-football-the-pitch-47730.jpeg?h=350&auto=compress")
 
-Photo.create(
+Photo.create!(
   user_id: guest.id,
   image_url: "https://images.pexels.com/photos/111085/pexels-photo-111085.jpeg?h=350&auto=compress",
   caption: "Why am I holding a surfboard?! There aren't any waves...")
 
-Photo.create(
+Photo.create!(
   user_id: sports_news.id,
   image_url: "https://images.pexels.com/photos/106218/pexels-photo-106218.jpeg?h=350&auto=compress&cs=tinysrgb",
   caption: "Almost time for baseball season.")

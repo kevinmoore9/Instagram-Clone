874cd415c5a0e05caa51ae9c348d8dfff519d3de
Kevin <kevinmoore9@users.noreply.github.com>
2017-01-13 09:23:03 -0800

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+guest = User.create(username: "guest", password: "password",
+  profile_img_url: "https://images.pexels.com/photos/26504/pexels-photo.jpg?w=1260&h=750&auto=compress&cs=tinysrgb")
+
+photos = Photo.create(user_id: guest.id,
+  image_url: "https://images.pexels.com/photos/111085/pexels-photo-111085.jpeg?h=350&auto=compress",
+  caption: "Why am I holding a surfboard?! There aren't any waves...")

847f82816858a770fb139abcaa4c22fe2a6c56ff
Kevin <kevinmoore9@users.noreply.github.com>
2017-01-23 22:16:10 -0800

    https update

--- frontend/components/comments/comment_form.jsx
+++ frontend/components/comments/comment_form.jsx
@@ -82,8 +82,8 @@ class CommentForm extends React.Component {
           <img className="like-button"
                src={
                  this.props.photo.liked
-                    ? "http://res.cloudinary.com/dc4ro79uf/image/upload/v1485238048/heart_gdavjf.png"
-                    : "http://res.cloudinary.com/dc4ro79uf/image/upload/v1485238086/blackheart_dyacg4.png"
+                    ? "https://res.cloudinary.com/dc4ro79uf/image/upload/v1485238048/heart_gdavjf.png"
+                    : "https://res.cloudinary.com/dc4ro79uf/image/upload/v1485238086/blackheart_dyacg4.png"
                   }
 
                onClick={this.handleLike}
